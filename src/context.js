02b4535e853e68d109a7377b1f2d75f237a1de73
Shan Ruan <54857794+shanruan33@users.noreply.github.com>
2020-01-20 12:22:04 -0500

    Update context.js

--- src/context.js
+++ src/context.js
@@ -91,7 +91,7 @@ class RoomProvider extends Component {
         if (type !== 'all') {
             tempRooms = tempRooms.filter(room => room.type === type);
         }
-        if (capacity !== 'all') {
+        if (capacity !== 1) {
             tempRooms = tempRooms.filter(room => room.capacity === capacity);
         }
         tempRooms = tempRooms.filter(room => room.price <= price);
